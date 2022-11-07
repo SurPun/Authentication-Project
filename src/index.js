ba73fc04203a7583798fa5a8275aa9089d2ee4df
Suraj Pun <s_pun@outlook.com>
2022-11-07 00:07:45 +0000

    Heroku Deploy

--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ const PORT = process.env.PORT || 3333;
 console.log(process.env.DB_FILE, process.env.COOKIE_SECRET);
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+
+// Heroku Deploy
